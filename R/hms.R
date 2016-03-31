f2d8cee9812dd62d815ee745dc026eaa53f491c7
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-31 12:11:57 +0200

    document

--- R/hms.R
+++ R/hms.R
@@ -1,5 +1,30 @@
+#' A simple class for storing time-of-day values
+#'
+#' The values are stored as a numeric vector that contains the number of seconds
+#' since midnight.  Supports construction from time values, coercion to and from
+#' various data types, and formatting.  Can be used in a data frame.
+#'
+#' @name hms
+#' @examples
+#' hms(56, 34, 12)
+#' as.hms(1)
+#' as.hms("12:34:56")
+#' as.hms(Sys.time())
+#' as.POSIXct(hms(1))
+#' \dontrun{
+#'   # Will raise an error
+#'   data.frame(a = hms(1))
+#' }
+#' d <- data.frame(hours = 1:3)
+#' d$b <- hms(hours = d$hours)
+#' d
+NULL
+
 # Construction ------------------------------------------------------------
 
+#' @rdname hms
+#' @param seconds,minutes,hours,days Time since midnight. No bounds checking is
+#'   performed.
 #' @export
 hms <- function(seconds = 0, minutes = 0, hours = 0, days = 0) {
   if (missing(seconds) && missing(minutes) && missing(hours) && missing(days)) {
@@ -12,26 +37,33 @@ hms <- function(seconds = 0, minutes = 0, hours = 0, days = 0) {
 }
 # Coercion in -------------------------------------------------------------
 
+#' @rdname hms
+#' @param x An object.
+#' @param ... Arguments passed on to further methods.
 #' @export
 as.hms <- function(x, ...) UseMethod("as.hms", x)
 
+#' @rdname hms
 #' @export
 as.hms.default <- function(x, ...) {
   stop("Can't convert object of class ", paste(class(x), collapse = ", "),
        " to hms.", call. = FALSE)
 }
 
+#' @rdname hms
 #' @export
 as.hms.numeric <- function(x, ...) hms(days = x)
 
 DATE_TIME_ORIGIN <- as.numeric(lubridate::parse_date_time("00:00:00", "hms"))
 
+#' @rdname hms
 #' @export
 as.hms.character <- function(x, ...) {
   seconds <- as.numeric(lubridate::parse_date_time(x, "hms")) - DATE_TIME_ORIGIN
   hms(seconds = seconds)
 }
 
+#' @rdname hms
 #' @export
 as.hms.POSIXt <- function(x, ...) {
   seconds <- as.numeric(as.POSIXct(x)) %% 86400
@@ -41,16 +73,19 @@ as.hms.POSIXt <- function(x, ...) {
 
 # Coercion out ------------------------------------------------------------
 
+#' @rdname hms
 #' @export
 as.POSIXct.hms <- function(x, ...) {
   structure(as.numeric(x), tzone = "UTC", class = c("POSIXct", "POSIXt"))
 }
 
+#' @rdname hms
 #' @export
 as.POSIXlt.hms <- function(x, ...) {
   as.POSIXlt(as.POSIXct(x, ...), ...)
 }
 
+#' @rdname hms
 #' @export
 as.character.hms <- function(x, ...) {
   strftime(as.POSIXct(x, ...), format = "%H:%M:%S", tz = "UTC")
@@ -59,11 +94,13 @@ as.character.hms <- function(x, ...) {
 
 # Output ------------------------------------------------------------------
 
+#' @rdname hms
 #' @export
 format.hms <- function(x, ...) {
   as.character(x)
 }
 
+#' @rdname hms
 #' @export
 print.hms <- function(x, ...) {
   cat(format(x), sep = "\n")
--- tests/testthat/test-construct.R
+++ tests/testthat/test-construct.R
@@ -3,6 +3,8 @@ context("construct")
 test_that("constructor", {
   expect_identical(hms(1:3, 2:4, 3:5),
                    hms(seconds = 1:3 + 2:4 * 60 + 3:5 * 3600))
+  expect_identical(hms(-1, 1), hms(59))
+  expect_identical(hms(3600), hms(hours = 1))
 
   expect_error(hms(), "seconds")
 })
