4a055b2be92d58e203cbae6f0758dafca5988cfe
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-14 16:05:35 +0200

    limit output to 6 digits, but print trailing zeros if rounded

--- R/format.R
+++ R/format.R
@@ -4,5 +4,8 @@ format_two_digits <- function(x) {
 
 format_split_seconds <- function(x) {
   split_second <- split_second_of_second(x)
-  gsub("^0", "", format(split_second, scientific = FALSE))
+  out <- format(split_second, scientific = FALSE)
+  digits <- max(min(max(nchar(out) - 2), 6), 0)
+  out <- formatC(split_second, format = "f", digits = digits)
+  gsub("^0", "", out)
 }
