f649b78cedf21c5d125e9c03ba6b2fca3edaa117
Kirill Müller <krlmlr+r@mailbox.org>
2017-07-25 13:32:30 +0200

    fix and enhance examples
    - Fix and enhance examples in `?hms`.

--- R/hms.R
+++ R/hms.R
@@ -12,14 +12,12 @@ setOldClass(c("hms", "difftime"))
 #' @name hms
 #' @examples
 #' hms(56, 34, 12)
+#' hms()
 #' as.hms(1)
 #' as.hms("12:34:56")
 #' as.hms(Sys.time())
 #' as.POSIXct(hms(1))
-#' \dontrun{
-#'   # Will raise an error
-#'   data.frame(a = hms(1))
-#' }
+#' data.frame(a = hms(1))
 #' d <- data.frame(hours = 1:3)
 #' d$hours <- hms(hours = d$hours)
 #' d
