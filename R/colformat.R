539f7cd217e72f0ffc7b5470cd98a923567a53ca
Kirill Müller <krlmlr+r@mailbox.org>
2017-09-12 13:23:22 +0200

    oops

--- R/colformat.R
+++ R/colformat.R
@@ -36,10 +36,13 @@ pillar_shaft.hms <- function(x, ...) {
       if (need_sign) ifelse(xx$sign, "-", " ") else "",
       pillar::style_num(format_two_digits(xx$minute_of_hour), xx$sign, highlight_minutes),
       pillar::style_subtle("'"),
-      pillar::style_num(format_two_digits(xx$second_of_minute), xx$sign, highlight_seconds),
-      pillar::style_num(format_split_seconds(xx$split_seconds), xx$sign, highlight_split_seconds),
+      pillar::style_num(format_two_digits(xx$second_of_minute), xx$sign, highlight_seconds)
     )
-    data <- paste0(data_seconds, pillar::style_subtle('"'))
+    data <- paste0(
+      data_seconds,
+      pillar::style_num(format_split_seconds(xx$split_seconds), xx$sign, highlight_split_seconds),
+      pillar::style_subtle('"')
+    )
   }
 
   na_indent <- crayon::col_nchar(data_seconds[1], type = "width") - 2L
