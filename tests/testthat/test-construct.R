26c6aa0e9e79988c179b46c87a40d95c05e92fc6
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-15 21:20:16 +0100

    fix hms() when passing all components

--- tests/testthat/test-construct.R
+++ tests/testthat/test-construct.R
@@ -1,8 +1,8 @@
 context("construct")
 
 test_that("constructor", {
-  expect_identical(hms(1:3, 2:4, 3:5),
-                   hms(seconds = 1:3 + 2:4 * 60 + 3:5 * 3600))
+  expect_identical(hms(1:3, 2:4, 3:5, 4:6),
+                   hms(seconds = 1:3 + 2:4 * 60 + 3:5 * 3600 + 4:6 * 86400))
   expect_identical(hms(-1, 1), hms(59))
   expect_identical(hms(3600), hms(hours = 1))
 
