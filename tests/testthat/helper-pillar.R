1fdef91c75e5bcd384fc3b1b01f887e30acd2d19
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-15 20:21:38 +0100

    further simplification due to improved pillar

--- tests/testthat/helper-pillar.R
+++ tests/testthat/helper-pillar.R
@@ -1,4 +1,4 @@
 expect_known_pillar_shaft_display <- function(x, file, width) {
-  object_quo <- rlang::quo(format(pillar::pillar_shaft(x), width = width))
+  object_quo <- rlang::quo(pillar::pillar_shaft(x))
   pillar::expect_known_display(!!object_quo, file = file.path("out", file))
 }
--- tests/testthat/test-colformat.R
+++ tests/testthat/test-colformat.R
@@ -3,27 +3,22 @@ context("pillar")
 test_that("pillar", {
   expect_known_pillar_shaft_display(
     hms(c(-3600, -60, -1, -0.001, 0, 0.001, 1, 60, 3600, NA)),
-    width = 13,
     file = "hmss.txt"
   )
   expect_known_pillar_shaft_display(
     hms(c(-3600000, -3600, -60, -1, 0, 1, 60, 3600, 3600000, NA)),
-    width = 11,
     file = "hms.txt"
   )
   expect_known_pillar_shaft_display(
     hms(c(-3600, -60, 0, 60, 3600, NA)),
-    width = 6,
     file = "hm.txt"
   )
   expect_known_pillar_shaft_display(
     hms(c(-60, -1, 0, 1, 60, NA)),
-    width = 7,
     file = "ms.txt"
   )
   expect_known_pillar_shaft_display(
     hms(c(-60, -1, -0.001, 0, 0.001, 1, 60, NA)),
-    width = 11,
     file = "mss.txt"
   )
 })
