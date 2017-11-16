bb0e0e15bfd04f0180ea97dd969b4ed8c05676f4
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-16 02:09:46 +0100

    skip tests failing for older versions

--- tests/testthat/test-combine.R
+++ tests/testthat/test-combine.R
@@ -6,14 +6,13 @@ test_that("combination keeps class and order", {
 
 test_that("combination coerces to hms", {
   expect_identical(c(hms(1), 2), hms(1:2))
+  if (getRversion() < "3.3") skip("Only for R >= 3.3")
   expect_identical(c(hms(1), "00:00:02"), hms(1:2))
 })
 
 
 # In R base,`c(as.difftime("20:00:00"), NA)` fails
 test_that("composition with NA fails", {
- expect_error(c(hms(1), NA))
+  if (getRversion() < "3.3") skip("Only for R >= 3.3")
+  expect_error(c(hms(1), NA))
 })
-
-
-
