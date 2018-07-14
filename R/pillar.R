1b247f51f765d246fa04fb014bd7f6bde17b524a
Kirill Müller <krlmlr+r@mailbox.org>
2018-07-14 23:22:08 +0200

    - Always show seconds in a pillar if they are different from zero (#54).
    Closes #54.

--- R/pillar.R
+++ R/pillar.R
@@ -3,18 +3,29 @@ pillar_shaft.hms <- function(x, ...) {
   data <- rep(NA_character_, length(x))
 
   xx <- decompose(x)
-  highlight_hours <- xx$hours > 0
+  has_hours <- xx$hours > 0
+  highlight_hours <- has_hours
   highlighted <- highlight_hours
-  highlight_minutes <- !highlighted & xx$minute_of_hour > 0
-  highlighted <- highlighted | highlight_minutes
-  highlight_seconds <- !highlighted & xx$second_of_minute > 0
-  highlighted <- highlighted | highlight_seconds
-  highlight_split_seconds <- !highlighted & xx$split_seconds > 0
 
-  need_split_seconds <- any(highlight_split_seconds, na.rm = TRUE)
-  need_seconds <- need_split_seconds || any(highlight_seconds, na.rm = TRUE)
-  need_hours <- any(highlight_hours, na.rm = TRUE)
+  has_minutes <- xx$minute_of_hour > 0
+  highlight_minutes <- !highlighted & has_minutes
+  highlighted <- highlighted | highlight_minutes
+
+  has_seconds <- xx$second_of_minute > 0
+  highlight_seconds <- !highlighted & has_seconds
+  highlighted <- highlighted | highlight_seconds
+
+  has_split_seconds <- xx$split_seconds > 0
+  highlight_split_seconds <- !highlighted & has_split_seconds
+
+  need_split_seconds <- any(has_split_seconds, na.rm = TRUE)
+  need_seconds <- need_split_seconds || any(has_seconds, na.rm = TRUE)
+  need_hours <- any(has_hours, na.rm = TRUE)
   need_sign <- any(xx$sign)
+
+  print(highlight_hours)
+  print(highlight_minutes)
+  print(highlight_seconds)
 
   if (need_hours) {
     data_seconds <- paste0(
