f9260540529e2b99667a928199f00c7dd5db6fca
Kirill Müller <krlmlr+r@mailbox.org>
2019-04-07 00:03:44 +0200

    Use vec_data() instead of as.numeric()

--- R/arith.R
+++ R/arith.R
@@ -46,9 +46,7 @@ tic_of_second <- function(x) {
 }
 
 decompose <- function(x) {
-  x <- vec_data(x)
-
-  x <- as.numeric(x) * TICS_PER_SECOND
+  x <- vec_data(x) * TICS_PER_SECOND
 
   # #140
   xr <- round(x)
