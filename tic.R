e12cee61d2c2afb3537f0c098cf265bb6fb0b9df
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-05 12:42:05 +0200

    Only deploy on master

--- tic.R
+++ tic.R
@@ -5,7 +5,7 @@ if (ci_has_env("DEV_VERSIONS")) {
     add_step(step_install_github(c("r-lib/rlang", "r-lib/pkgconfig", "r-lib/vctrs")))
 }
 
-if (ci_has_env("BUILD_PKGDOWN")) {
+if (ci_has_env("BUILD_PKGDOWN") && ci_get_branch() == "master") {
   get_stage("install") %>%
     add_step(step_install_github("tidyverse/tidytemplate"))
 
