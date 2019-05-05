773d3a94160d171e81e0051bdfeb24c2329e54a0
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-05 20:43:10 +0200

    Need utils

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 .onLoad <- function(...) {
   vctrs::s3_register("pillar::pillar_shaft", "hms")
 
-  if (packageVersion("vctrs") <= "0.1.0") {
+  if (utils::packageVersion("vctrs") <= "0.1.0") {
     vec_default_cast <<- vec_default_cast_old
   } else {
     rm("vec_default_cast", inherits = TRUE)
