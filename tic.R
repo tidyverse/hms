1cc90c7e58f4bbe05860525ddd4f46ed325e53d3
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-05 12:37:46 +0200

    Update CI

--- tic.R
+++ tic.R
@@ -1,6 +1,10 @@
-add_package_checks()
+do_package_checks()
 
 if (ci_has_env("DEV_VERSIONS")) {
   get_stage("install") %>%
-    add_step(step_install_github("r-lib/vctrs"))
+    add_step(step_install_github(c("r-lib/rlang", "r-lib/pkgconfig", "r-lib/vctrs")))
+}
+
+if (ci_has_env("BUILD_PKGDOWN")) {
+  do_pkgdown()
 }
