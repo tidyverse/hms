efc27cd63644ee6b332d742e1f03f812e68e02ca
Kirill Müller <krlmlr+r@mailbox.org>
2019-06-19 23:07:44 +0200

    Use expect_s3_class()

--- tests/testthat/helper-compare.R
+++ tests/testthat/helper-compare.R
@@ -1,11 +1,11 @@
 expect_hms_equal <- function(x, y) {
-  expect_is(x, "hms")
-  expect_is(y, "hms")
+  expect_s3_class(x, "hms")
+  expect_s3_class(y, "hms")
   expect_equal(as.numeric(x), as.numeric(y))
 }
 
 expect_difftime_equal <- function(x, y) {
-  expect_is(x, "difftime")
-  expect_is(y, "difftime")
+  expect_s3_class(x, "difftime")
+  expect_s3_class(y, "difftime")
   expect_equal(as.numeric(as_hms(x)), as.numeric(as_hms(y)))
 }
