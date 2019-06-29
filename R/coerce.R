44962bd905c6c0297c100f53b1b6c331f5c64a16
Kirill Müller <krlmlr+r@mailbox.org>
2019-06-29 23:42:26 +0200

    Adapt to dev vctrs

--- R/coerce.R
+++ R/coerce.R
@@ -1,63 +1,63 @@
 #' Coercion
 #'
-#' Double dispatch methods to support [vctrs::vec_type2()].
+#' Double dispatch methods to support [vctrs::vec_ptype2()].
 #'
-#' @inheritParams vctrs::vec_type2
+#' @inheritParams vctrs::vec_ptype2
 #'
-#' @method vec_type2 hms
+#' @method vec_ptype2 hms
 #' @export
-#' @export vec_type2.hms
-vec_type2.hms <- function(x, y, ..., x_arg = "", y_arg = "") UseMethod("vec_type2.hms", y)
+#' @export vec_ptype2.hms
+vec_ptype2.hms <- function(x, y, ..., x_arg = "", y_arg = "") UseMethod("vec_ptype2.hms", y)
 
-#' @method vec_type2.hms default
+#' @method vec_ptype2.hms default
 #' @export
-vec_type2.hms.default <- function(x, y, ..., x_arg = "", y_arg = "") stop_incompatible_type(x, y, x_arg, y_arg)
+vec_ptype2.hms.default <- function(x, y, ..., x_arg = "", y_arg = "") stop_incompatible_type(x, y, x_arg, y_arg)
 
-#' @method vec_type2.hms vctrs_unspecified
+#' @method vec_ptype2.hms vctrs_unspecified
 #' @export
-vec_type2.hms.vctrs_unspecified <- function(x, y, ...) x
+vec_ptype2.hms.vctrs_unspecified <- function(x, y, ...) x
 
-#' @method vec_type2.hms hms
+#' @method vec_ptype2.hms hms
 #' @export
-vec_type2.hms.hms <- function(x, y, ...) hms::hms()
+vec_ptype2.hms.hms <- function(x, y, ...) hms::hms()
 
-#' @method vec_type2.difftime hms
+#' @method vec_ptype2.difftime hms
 #' @export
-vec_type2.difftime.hms <- function(x, y, ...) new_hms()
+vec_ptype2.difftime.hms <- function(x, y, ...) new_hms()
 
-#' @method vec_type2.hms difftime
+#' @method vec_ptype2.hms difftime
 #' @export
-vec_type2.hms.difftime <- function(x, y, ...) new_hms()
+vec_ptype2.hms.difftime <- function(x, y, ...) new_hms()
 
-#' @method vec_type2.double hms
+#' @method vec_ptype2.double hms
 #' @export
-vec_type2.double.hms <- function(x, y, ...) {
+vec_ptype2.double.hms <- function(x, y, ...) {
   warn_deprecated("Coercion of <double> to <time> is deprecated and will be removed in a future version. Please use as_hms().")
   new_hms()
 }
 
-#' @method vec_type2.hms double
+#' @method vec_ptype2.hms double
 #' @export
-vec_type2.hms.double <- function(x, y, ...) vec_type2.double.hms(y, x)
+vec_ptype2.hms.double <- function(x, y, ...) vec_ptype2.double.hms(y, x)
 
-#' @method vec_type2.integer hms
+#' @method vec_ptype2.integer hms
 #' @export
-vec_type2.integer.hms <- function(x, y, ...) {
+vec_ptype2.integer.hms <- function(x, y, ...) {
   warn_deprecated("Coercion of <integer> to <time> is deprecated and will be removed in a future version. Please use as_hms().")
   new_hms()
 }
 
-#' @method vec_type2.hms integer
+#' @method vec_ptype2.hms integer
 #' @export
-vec_type2.hms.integer <- function(x, y, ...) vec_type2.integer.hms(y, x)
+vec_ptype2.hms.integer <- function(x, y, ...) vec_ptype2.integer.hms(y, x)
 
-#' @method vec_type2.character hms
+#' @method vec_ptype2.character hms
 #' @export
-vec_type2.character.hms <- function(x, y, ...) {
+vec_ptype2.character.hms <- function(x, y, ...) {
   warn_deprecated("Coercion of <character> to <time> is deprecated and will be removed in a future version. Please use as_hms().")
   new_hms()
 }
 
-#' @method vec_type2.hms character
+#' @method vec_ptype2.hms character
 #' @export
-vec_type2.hms.character <- function(x, y, ...) vec_type2.character.hms(y, x)
+vec_ptype2.hms.character <- function(x, y, ...) vec_ptype2.character.hms(y, x)
