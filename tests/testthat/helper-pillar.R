e5fe876c6a2ff955abb0d92e1d5a155ebde8d368
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-06 11:08:13 +0200

    - Don't test colored output on CRAN.

--- tests/testthat/helper-pillar.R
+++ tests/testthat/helper-pillar.R
@@ -1,4 +1,8 @@
 expect_known_pillar_shaft_display <- function(x, file, width) {
   object_quo <- rlang::quo(pillar::pillar_shaft(x))
-  pillar::expect_known_display(!!object_quo, file = file.path("out", file))
+
+  # Avoid crayon tests on CRAN
+  crayon <- identical(Sys.getenv("NOT_CRAN"), "true")
+
+  pillar::expect_known_display(!!object_quo, file = file.path("out", file), crayon = crayon)
 }
