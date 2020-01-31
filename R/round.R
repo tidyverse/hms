ec65c76472d596c632ac2505f670b4e5447b1285
Hunter Glanz <hglanz@calpoly.edu>
2020-01-31 11:45:00 -0800

    Initial addtion of digits argument
    still need error messages and possible documentation

--- R/round.R
+++ R/round.R
@@ -4,12 +4,27 @@
 #' @param x A vector of class [hms]
 #' @param secs Multiple of seconds, a positive numeric. Values less than one
 #'   are supported
-#' @return The input, rounded or truncated to the nearest multiple of `secs`
+#' @param digits Number of digits, a whole number between -100 and 100.
+#' @return The input, rounded or truncated to the nearest multiple of `secs` (or number of `digits`)
 #' @export
 #' @examples
 #' round_hms(as_hms("12:34:56"), 5)
 #' round_hms(as_hms("12:34:56"), 60)
-round_hms <- function(x, secs) {
+#' round_hms(as_hms("12:34:56"), digits = 1)
+#' round_hms(as_hms("12:34:56"), digits = -2)
+round_hms <- function(x, secs = NULL, digits = NULL) {
+  if (!is.null(digits)) {
+    if (d %% 1 == 0) {
+      if (d <= 100 & d >= -100) {
+        secs <- digits2sec(digits)
+      } else {
+        # error message here
+      }
+    } else {
+      # error message here
+    }
+  }
+
   vec_restore(round(as.numeric(x) / secs) * secs, x)
 }
 
@@ -17,6 +32,29 @@ round_hms <- function(x, secs) {
 #' @export
 #' @examples
 #' trunc_hms(as_hms("12:34:56"), 60)
-trunc_hms <- function(x, secs) {
+trunc_hms <- function(x, secs = NULL, digits = NULL) {
+  if (!is.null(digits)) {
+    if (d %% 1 == 0) {
+      if (d <= 100 & d >= -100) {
+        secs <- digits2sec(digits)
+      } else {
+        # error message here
+      }
+    } else {
+      # error message here
+    }
+  }
+
   vec_restore(trunc(as.numeric(x) / secs) * secs, x)
+}
+
+
+digits2sec <- function(digits) {
+  if (digits >= -1) {
+    secs <- 10 ^ -digits
+  } else if (digits < -1) {
+    secs <- 60 ^ (-digits - 1)
+  }
+
+  return(secs)
 }
--- tests/testthat/test-round.R
+++ tests/testthat/test-round.R
@@ -3,6 +3,8 @@ context("round")
 test_that("round_hms", {
   expect_equal(round_hms(parse_hms("12:34:56"), 5), hms(55, 34, 12))
   expect_equal(round_hms(parse_hms("12:34:56"), 60), hms(0, 35, 12))
+  expect_equal(round_hms(parse_hms("12:34:56"), digits = 1), hms(56, 34, 12))
+  expect_equal(round_hms(parse_hms("12:34:56"), digits = -2), hms(0, 35, 12))
   expect_equal(round_hms(hms(0.7), 0.25), hms(0.75))
   expect_equal(round_hms(hms(NA), 5), hms(NA))
   expect_equal(round_hms(parse_hms(c("12:34:56", NA)), 5), as_hms(c(hms(55, 34, 12), hms(NA))))
@@ -11,6 +13,8 @@ test_that("round_hms", {
 test_that("trunc_hms", {
   expect_equal(trunc_hms(parse_hms("12:34:56"), 5), hms(55, 34, 12))
   expect_equal(trunc_hms(parse_hms("12:34:56"), 60), hms(0, 34, 12))
+  expect_equal(trunc_hms(parse_hms("12:34:56"), digits = 1), hms(56, 34, 12))
+  expect_equal(trunc_hms(parse_hms("12:34:56"), digits = -2), hms(0, 34, 12))
   expect_equal(trunc_hms(hms(0.7), 0.25), hms(0.5))
   expect_equal(trunc_hms(hms(NA), 5), hms(NA))
   expect_equal(trunc_hms(parse_hms(c("12:34:56", NA)), 5), as_hms(c(hms(55, 34, 12), hms(NA))))
