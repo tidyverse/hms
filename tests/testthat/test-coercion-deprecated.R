366ec8ae7a9654ed7f9b57a89f8ace1d00cdb66b
Kirill Müller <krlmlr+r@mailbox.org>
2021-09-26 20:27:06 +0200

    expect_deprecated()

--- tests/testthat/test-coercion-deprecated.R
+++ tests/testthat/test-coercion-deprecated.R
@@ -1,24 +1,30 @@
 test_that("coercion in", {
-  expect_identical(expect_deprecated(as.hms(0.5 * 86400)), hms(hours = 12))
-  expect_identical(expect_deprecated(as.hms(-0.25 * 86400)), hms(hours = -6))
-  expect_hms_equal(expect_deprecated(as.hms("12:34:56")), hms(56, 34, 12))
-  expect_hms_equal(
-    expect_deprecated(as.hms(strptime("12:34:56", format = "%H:%M:%S", tz = "UTC"), tz = "UTC")),
-    hms(56, 34, 12)
+  expect_deprecated(expect_identical(as.hms(0.5 * 86400), hms(hours = 12)))
+  expect_deprecated(expect_identical(as.hms(-0.25 * 86400), hms(hours = -6)))
+  expect_deprecated(expect_hms_equal(as.hms("12:34:56"), hms(56, 34, 12)))
+  expect_deprecated(
+    expect_hms_equal(
+      as.hms(strptime("12:34:56", format = "%H:%M:%S", tz = "UTC"), tz = "UTC"),
+      hms(56, 34, 12)
+    )
   )
-  expect_hms_equal(
-    expect_deprecated(as.hms(strptime("12:34:56", format = "%H:%M:%S", tz = "Europe/Zurich"), tz = "Europe/Zurich")),
-    hms(56, 34, 12)
+  expect_deprecated(
+    expect_hms_equal(
+      as.hms(strptime("12:34:56", format = "%H:%M:%S", tz = "Europe/Zurich"), tz = "Europe/Zurich"),
+      hms(56, 34, 12)
+    )
   )
-  expect_hms_equal(
-    expect_deprecated(as.hms(strptime("12:34:56", format = "%H:%M:%S", tz = "PST8PDT"), tz = "PST8PDT")),
-    hms(56, 34, 12)
+  expect_deprecated(
+    expect_hms_equal(
+      as.hms(strptime("12:34:56", format = "%H:%M:%S", tz = "PST8PDT"), tz = "PST8PDT"),
+      hms(56, 34, 12)
+    )
   )
 
   now <- Sys.time()
   now_lt <- as.POSIXlt(now)
-  expect_hms_equal(expect_deprecated(as.hms(now)), hms(now_lt$sec, now_lt$min, now_lt$hour))
-  expect_hms_equal(expect_deprecated(as.hms(now_lt)), as_hms(now))
+  expect_deprecated(expect_hms_equal(as.hms(now), hms(now_lt$sec, now_lt$min, now_lt$hour)))
+  expect_deprecated(expect_hms_equal(as.hms(now_lt), as_hms(now)))
 
   expect_error(expect_deprecated(as.hms(FALSE)))
 })
