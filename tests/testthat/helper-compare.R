8759889cc254fe045898a7b3c7d0475d24f65f43
Alexander Rosenstock <alexander.rosenstock@web.de>
2023-08-24 19:00:52 +0200

    revert removal of expect_difftime_equal() test helper for R < 4.1

--- tests/testthat/helper-compare.R
+++ tests/testthat/helper-compare.R
@@ -3,3 +3,9 @@ expect_hms_equal <- function(x, y) {
   expect_s3_class(y, "hms")
   expect_equal(as.numeric(x), as.numeric(y))
 }
+
+expect_difftime_equal <- function(x, y) {
+  expect_s3_class(x, "difftime")
+  expect_s3_class(y, "difftime")
+  expect_equal(as.numeric(as_hms(x)), as.numeric(as_hms(y)))
+}
--- tests/testthat/test-arith.R
+++ tests/testthat/test-arith.R
@@ -11,11 +11,19 @@ test_that("arithmetics work", {
   expect_equal(hms(days = 1) + as.Date("2016-03-31"), as.Date("2016-04-01"))
   expect_equal(empty_tz(hms(hours = 1) + as.POSIXct("2016-03-31")), as.POSIXct("2016-03-31 01:00:00"))
 
-  expect_hms_equal(hms(1) + hms(2), hms(3))
-  expect_hms_equal(hms(1) - hms(2), hms(-1))
-  expect_hms_equal(2 * hms(1), hms(2))
-  expect_hms_equal(hms(hours = 1) / 2, hms(minutes = 30))
-  expect_hms_equal(-hms(1), hms(-1))
+  if (getRversion() < "4.1") {
+    expect_difftime_equal(hms(1) + hms(2), hms(3))
+    expect_difftime_equal(hms(1) - hms(2), hms(-1))
+    expect_difftime_equal(2 * hms(1), hms(2))
+    expect_difftime_equal(hms(hours = 1) / 2, hms(minutes = 30))
+    expect_difftime_equal(-hms(1), hms(-1))
+  } else {
+    expect_hms_equal(hms(1) + hms(2), hms(3))
+    expect_hms_equal(hms(1) - hms(2), hms(-1))
+    expect_hms_equal(2 * hms(1), hms(2))
+    expect_hms_equal(hms(hours = 1) / 2, hms(minutes = 30))
+    expect_hms_equal(-hms(1), hms(-1))
+  }
 })
 
 test_that("component extraction work", {
